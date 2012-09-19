1cf9dbe1c127ac14cd2317a70b0d305c25141880
Ryan Fiorini <ryanwfiorini@gmail.com>
2012-09-19 16:05:31 -0400

    renamed files

---
+++ Scripts/jquery.wiggler.js
@@ -0,0 +1,21 @@
+﻿(function ($) {
+    $.fn.extend({
+        //plugin name - animatemenu
+        wiggler: function (options) {
+            var defaults = {};
+            var options = $.extend(defaults, options);
+
+            return this.each(function () {
+                $(this).mouseover(function () {
+                    //$(this).animate({ paddingLeft: o.animatePadding }, 300);
+                    $(this).addClass("animation-time wiggle");
+                    //$(this).css("right", "50px").css("left", "50px");
+                }).mouseout(function () {
+                    //$(this).animate({ paddingLeft: o.defaultPadding }, 300);
+                    $(this).removeClass("animation-time wiggle");
+                });
+
+            });
+        }
+    });
+})(jQuery);
