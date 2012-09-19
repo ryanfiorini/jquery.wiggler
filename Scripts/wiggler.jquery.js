9003e675e0dad6c3f41dcf8aa7e077d71f6bf739
Ryan Fiorini <ryanwfiorini@gmail.com>
2012-09-19 16:01:10 -0400

    minor changes

--- Scripts/wiggler.jquery.js
+++ Scripts/wiggler.jquery.js
@@ -2,20 +2,17 @@
     $.fn.extend({
         //plugin name - animatemenu
         wiggler: function (options) {
-
-            //Settings list and the default values
             var defaults = {};
-
             var options = $.extend(defaults, options);
 
             return this.each(function () {
-                //Attach mouseover and mouseout event to the LI  
                 $(this).mouseover(function () {
                     //$(this).animate({ paddingLeft: o.animatePadding }, 300);
-                    $(this).addClass("animation-time shake");
+                    $(this).addClass("animation-time wiggle");
+                    //$(this).css("right", "50px").css("left", "50px");
                 }).mouseout(function () {
                     //$(this).animate({ paddingLeft: o.defaultPadding }, 300);
-                    $(this).removeClass("animation-time shake");
+                    $(this).removeClass("animation-time wiggle");
                 });
 
             });
