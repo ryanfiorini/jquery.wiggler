6f058c7f9b0183cceb9fce7667f20eabb8d56817
Ryan Fiorini <ryanwfiorini@gmail.com>
2012-09-19 07:53:09 -0400

    init of wiggler

---
+++ Scripts/wiggler.jquery.js
@@ -0,0 +1,24 @@
+﻿(function ($) {
+    $.fn.extend({
+        //plugin name - animatemenu
+        wiggler: function (options) {
+
+            //Settings list and the default values
+            var defaults = {};
+
+            var options = $.extend(defaults, options);
+
+            return this.each(function () {
+                //Attach mouseover and mouseout event to the LI  
+                $(this).mouseover(function () {
+                    //$(this).animate({ paddingLeft: o.animatePadding }, 300);
+                    $(this).addClass("animation-time shake");
+                }).mouseout(function () {
+                    //$(this).animate({ paddingLeft: o.defaultPadding }, 300);
+                    $(this).removeClass("animation-time shake");
+                });
+
+            });
+        }
+    });
+})(jQuery);
